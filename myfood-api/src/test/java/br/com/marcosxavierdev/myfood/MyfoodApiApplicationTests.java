643419df42931078fce892d426b9c25d73ae2154
Marcos.Xavier <@marcosxavierdev>
2021-09-05 09:55:42 -0300

    2.5 - criando o projeto com o spring boot

---
+++ myfood-api/src/test/java/br/com/marcosxavierdev/myfood/MyfoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcosxavierdev.myfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
